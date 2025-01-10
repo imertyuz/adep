4e798779e7a05eb323e9f1ed069a60966503cc44
imertyuz <irem.ozcan093@gmail.com>
2025-01-10 15:28:28 +0300

    Assro TF

--- launch/launch_robot.launch.py
+++ launch/launch_robot.launch.py
@@ -25,9 +25,14 @@ def generate_launch_description():
     rsp = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory(package_name),'launch','rsp.launch.py'
-                )]), launch_arguments={'use_sim_time': 'true' , 'use_ros2_control': 'true'}.items()
+                )]), launch_arguments={'use_sim_time': 'false' , 'use_ros2_control': 'true'}.items()
     )
-
+    
+    joystick = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory(package_name),'launch','joystick.launch.py'
+                )])
+    )
 
     robot_description = Command(['ros2 param get --hide-type /robot_state_publisher robot_description'])
     
@@ -88,6 +93,7 @@ def generate_launch_description():
     # Launch them all!
     return LaunchDescription([
         rsp,
+        joystick,
         delayed_controller_manager,
         delayed_diff_drive_spawner,
         delayed_joint_broad_spawner
